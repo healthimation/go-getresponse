c7c87e95fc8e74529826c4e602a0dc06290238ce
Kyle <kyle.boorky@gmail.com>
2017-06-30 14:06:51 -0400

    adding client contact functions

--- getresponse/client.go
+++ getresponse/client.go
@@ -2,12 +2,13 @@ package getresponse
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
+	"strings"
 	"time"
-
-	"encoding/json"
 
 	"github.com/healthimation/go-client/client"
 	"github.com/healthimation/go-glitch/glitch"
@@ -47,10 +48,10 @@ type Client interface {
 	CreateContact(ctx context.Context, email string, name *string, dayOfCycle *int32, campaignID string, customFields []CustomField, ipAddress *string) glitch.DataError
 
 	// GetContacts - https://apidocs.getresponse.com/v3/resources/contacts#contacts.get.all
-	GetContacts(ctx context.Context, queryHash map[string]string, fields *string, sortHash map[string]string, page int32, perPage int32, additionalFlags *string) ([]Contact, glitch.DataError)
+	GetContacts(ctx context.Context, queryHash map[string]string, fields []string, sortHash map[string]string, page int32, perPage int32, additionalFlags *string) ([]Contact, glitch.DataError)
 
 	// Get Contact - https://apidocs.getresponse.com/v3/resources/contacts#contacts.get
-	GetContact(ctx context.Context, ID string, fields *string) (Contact, glitch.DataError)
+	GetContact(ctx context.Context, ID string, fields []string) (Contact, glitch.DataError)
 
 	// UpdateContact - https://apidocs.getresponse.com/v3/resources/contacts#contacts.update
 	UpdateContact(ctx context.Context, ID string, newData Contact) (Contact, glitch.DataError)
@@ -79,6 +80,7 @@ func (g *getResponseClient) CreateContact(ctx context.Context, email string, nam
 	slug := "/v3/contacts"
 	h := http.Header{}
 	h.Set("Content-type", "application/json")
+	h.Set("X-Auth-Token", fmt.Sprintf("api-key %s", g.apiKey))
 
 	bodyObj := createContactRequest{
 		Email:             email,
@@ -94,10 +96,165 @@ func (g *getResponseClient) CreateContact(ctx context.Context, email string, nam
 		return err
 	}
 
-	query := url.Values{}
-	query.Set("api_key", p.apiKey)
+	status, ret, err := g.c.MakeRequest(ctx, http.MethodPost, slug, nil, h, body)
+	if err != nil {
+		return err
+	}
 
-	status, ret, err = g.c.MakeRequest(ctx, http.MethodPost, slug, query, h, body)
+	if status < 200 || status >= 400 {
+		//parse error
+		return g.parseError(ret)
+	}
+
+	return nil
+}
+
+func (g *getResponseClient) GetContacts(ctx context.Context, queryHash map[string]string, fields []string, sortHash map[string]string, page int32, perPage int32, additionalFlags *string) ([]Contact, glitch.DataError) {
+	slug := "/v3/contacts"
+	h := http.Header{}
+	h.Set("Content-type", "application/json")
+	h.Set("X-Auth-Token", fmt.Sprintf("api-key %s", g.apiKey))
+
+	query := url.Values{}
+	for k, v := range queryHash {
+		query.Set(fmt.Sprintf("query[%s]", k), v)
+	}
+
+	for k, v := range sortHash {
+		query.Set(fmt.Sprintf("sort[%s]", k), v)
+	}
+
+	if len(fields) > 0 {
+		query.Set("fields", strings.Join(fields, ","))
+	}
+
+	query.Set("page", strconv.Itoa(int(page)))
+	query.Set("perPage", strconv.Itoa(int(perPage)))
+
+	if additionalFlags != nil {
+		query.Set("additionalFlags", *additionalFlags)
+	}
+
+	result := make([]Contact, 0)
+	status, ret, err := g.c.MakeRequest(ctx, http.MethodGet, slug, query, h, nil)
+	if err != nil {
+		return result, err
+	}
+
+	if status < 200 || status >= 400 {
+		//parse error
+		return result, g.parseError(ret)
+	}
+
+	jErr := json.Unmarshal(ret, &result)
+	if jErr != nil {
+		return result, glitch.NewDataError(err, client.ErrorDecodingResponse, fmt.Sprintf("Could not unmarshal response: %s", ret))
+	}
+
+	return result, nil
+}
+
+func (g *getResponseClient) GetContact(ctx context.Context, ID string, fields []string) (Contact, glitch.DataError) {
+	slug := fmt.Sprintf("/v3/contacts/%s", ID)
+	h := http.Header{}
+	h.Set("Content-type", "application/json")
+	h.Set("X-Auth-Token", fmt.Sprintf("api-key %s", g.apiKey))
+
+	query := url.Values{}
+	if len(fields) > 0 {
+		query.Set("fields", strings.Join(fields, ","))
+	}
+
+	result := Contact{}
+	status, ret, err := g.c.MakeRequest(ctx, http.MethodGet, slug, query, h, nil)
+	if err != nil {
+		return result, err
+	}
+
+	if status < 200 || status >= 400 {
+		//parse error
+		return result, g.parseError(ret)
+	}
+
+	jErr := json.Unmarshal(ret, &result)
+	if jErr != nil {
+		return result, glitch.NewDataError(err, client.ErrorDecodingResponse, fmt.Sprintf("Could not unmarshal response: %s", ret))
+	}
+
+	return result, nil
+}
+
+func (g *getResponseClient) UpdateContact(ctx context.Context, ID string, newData Contact) (Contact, glitch.DataError) {
+	result := Contact{}
+	slug := fmt.Sprintf("/v3/contacts/%s", ID)
+	h := http.Header{}
+	h.Set("Content-type", "application/json")
+	h.Set("X-Auth-Token", fmt.Sprintf("api-key %s", g.apiKey))
+
+	body, err := client.ObjectToJSONReader(newData)
+	if err != nil {
+		return result, err
+	}
+
+	status, ret, err := g.c.MakeRequest(ctx, http.MethodPost, slug, nil, h, body)
+	if err != nil {
+		return result, err
+	}
+
+	if status < 200 || status >= 400 {
+		//parse error
+		return result, g.parseError(ret)
+	}
+
+	jErr := json.Unmarshal(ret, &result)
+	if jErr != nil {
+		return result, glitch.NewDataError(err, client.ErrorDecodingResponse, fmt.Sprintf("Could not unmarshal response: %s", ret))
+	}
+
+	return result, nil
+}
+
+func (g *getResponseClient) UpdateContactCustomFields(ctx context.Context, ID string, customFields []CustomField) (Contact, glitch.DataError) {
+	result := Contact{}
+	slug := fmt.Sprintf("/v3/contacts/%s/custom-fields", ID)
+	h := http.Header{}
+	h.Set("Content-type", "application/json")
+	h.Set("X-Auth-Token", fmt.Sprintf("api-key %s", g.apiKey))
+
+	body, err := client.ObjectToJSONReader(customFields)
+	if err != nil {
+		return result, err
+	}
+
+	status, ret, err := g.c.MakeRequest(ctx, http.MethodPost, slug, nil, h, body)
+	if err != nil {
+		return result, err
+	}
+
+	if status < 200 || status >= 400 {
+		//parse error
+		return result, g.parseError(ret)
+	}
+
+	jErr := json.Unmarshal(ret, &result)
+	if jErr != nil {
+		return result, glitch.NewDataError(err, client.ErrorDecodingResponse, fmt.Sprintf("Could not unmarshal response: %s", ret))
+	}
+
+	return result, nil
+}
+
+func (g *getResponseClient) DeleteContact(ctx context.Context, ID string, messageID string, ipAddress string) glitch.DataError {
+	slug := fmt.Sprintf("/v3/contacts/%s", ID)
+	h := http.Header{}
+	h.Set("Content-type", "application/json")
+	h.Set("X-Auth-Token", fmt.Sprintf("api-key %s", g.apiKey))
+
+	query := url.Values{}
+	query.Set("messageId", messageID)
+	query.Set("ipAddress", ipAddress)
+
+	status, ret, err := g.c.MakeRequest(ctx, http.MethodDelete, slug, query, h, nil)
 	if err != nil {
 		return err
 	}
